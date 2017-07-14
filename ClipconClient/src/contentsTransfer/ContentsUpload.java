cff96999fc0657725054a543057946c4f91057e5
kwonheejeong <gmlwjd9405@naver.com>
2017-07-14 18:38:02 +0900

    Implement upload and upload progress using Retrofit

--- ClipconClient/src/contentsTransfer/ContentsUpload.java
+++ ClipconClient/src/contentsTransfer/ContentsUpload.java
@@ -5,25 +5,29 @@ import java.util.ArrayList;
 
 import controller.ClipboardController;
 import controller.Endpoint;
+import retrofitContentsTransfer.RetrofitUploadTest;
 
 public class ContentsUpload {
-	 public static UploadData uploader;
-	 public static boolean isUpload = false;
-	 
-	 public void upload() {
-		 isUpload = true;
-		 uploader = new UploadData(Endpoint.user.getName(), Endpoint.user.getGroup().getPrimaryKey());
-		 
-		 Object clipboardData = ClipboardController.readClipboard();
-		 
-		 if (clipboardData instanceof String) {
-			 uploader.uploadStringData((String) clipboardData);
-	     } 
-		 else if (clipboardData instanceof Image) {
-			 uploader.uploadCapturedImageData((Image) clipboardData);
-	     }
-		 else if (clipboardData instanceof ArrayList<?>) {
-			 uploader.uploadMultipartData((ArrayList<String>) clipboardData);
-		 }
-	 }
+	public static UploadData uploader;
+	public static RetrofitUploadTest uploader2;
+	public static boolean isUpload = false;
+
+	public void upload() {
+		isUpload = true;
+		uploader = new UploadData(Endpoint.user.getName(), Endpoint.user.getGroup().getPrimaryKey());
+		uploader2 = new RetrofitUploadTest(Endpoint.user.getName(), Endpoint.user.getGroup().getPrimaryKey());
+
+		Object clipboardData = ClipboardController.readClipboard();
+
+		if (clipboardData instanceof String) {
+			uploader.uploadStringData((String) clipboardData);
+		} else if (clipboardData instanceof Image) {
+			uploader.uploadCapturedImageData((Image) clipboardData);
+		} else if (clipboardData instanceof ArrayList<?>) {
+			// uploader.uploadMultipartData((ArrayList<String>) clipboardData);
+			System.out.println("uploader2");
+			uploader2.uploadFile((ArrayList<String>) clipboardData);
+
+		}
+	}
 }
