ea85ab1028c1d2222227bc7fe8c0497d9e083aaa
delf <ttjs2008@naver.com>
2017-09-14 01:26:24 +0900

    Change resouce directory

--- ClipconClient/src/application/Main.java
+++ ClipconClient/src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
@@ -10,6 +11,7 @@ import java.util.Date;
 
 import javax.websocket.EncodeException;
 
+import authority.Elevator;
 import controller.ClipboardController;
 import controller.Endpoint;
 import javafx.application.Application;
@@ -30,7 +32,7 @@ public class Main extends Application {
 
 	public static final String SERVER_PORT = "80";
 	public static final String SERVER_ADDR = "113.198.84.53";
-	//public static final String SERVER_ADDR = "delf.gonetis.com";
+	// public static final String SERVER_ADDR = "delf.gonetis.com";
 	// public static final String SERVER_ADDR = "223.194.156.74";
 
 	public static final String SERVER_URI_PART = SERVER_ADDR + ":" + SERVER_PORT + "/";
@@ -38,7 +40,7 @@ public class Main extends Application {
 	public final String CLIPCON_VERSION = "1.1";
 
 	public static final String LOCK_FILE_LOCATION = System.getProperty("user.dir") + File.separator + "ClipCon.lock";
-	private File lockFile = new File(Main.LOCK_FILE_LOCATION);
+	private static File lockFile;
 
 	public static boolean isInMainScene = false;
 
@@ -47,7 +49,7 @@ public class Main extends Application {
 	private static HostServices hostService;
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void start(Stage primaryStage) throws Exception { // 이거 실행되는거야?
 
 		hostService = getHostServices();
 
@@ -59,18 +61,6 @@ public class Main extends Application {
 			endpoint.sendMessage(confirmVersionMsg);
 		} catch (IOException | EncodeException e) {
 			e.printStackTrace();
-		}
-
-		@SuppressWarnings("resource")
-		FileChannel channel = new RandomAccessFile(lockFile, "rw").getChannel();
-
-		// try lock
-		FileLock lock = channel.tryLock();
-
-		// already obtain lock in other JVM
-		if (lock == null) {
-			channel.close();
-			System.exit(0);
 		}
 		try {
 			System.load(System.getProperty("user.dir") + File.separator + "keyHooking.dll");
@@ -91,7 +81,7 @@ public class Main extends Application {
 		Parent root = FXMLLoader.load(getClass().getResource("/view/StartingView.fxml"));
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
-//		primaryStage.setAlwaysOnTop(false);
+		// primaryStage.setAlwaysOnTop(false);
 		primaryStage.setResizable(true);
 		primaryStage.show();
 
@@ -133,16 +123,52 @@ public class Main extends Application {
 		return Main.hostService;
 	}
 
-	public static void main(String[] args) {
-		launch(args);
+	@SuppressWarnings("resource")
+	public static void fileLock() throws FileNotFoundException {
+		lockFile = new File(Main.LOCK_FILE_LOCATION);
+
+		FileChannel channel;
+		channel = new RandomAccessFile(lockFile, "rw").getChannel();
+
+		// try lock
+		FileLock lock;
+		try {
+			lock = channel.tryLock();
+			// already obtain lock in other JVM
+			if (lock == null) {
+				channel.close();
+				System.exit(0);
+			}
+		} catch (IOException e) {
+			// error handle
+		}
 	}
-	
-	/** for test 17.09.13 
-	 * @return Current Time YYYY-MM-DD HH:MM:SS */
+
+	public static void main(String[] args) {
+		try {
+			fileLock();
+			launch(args);
+		} catch (FileNotFoundException e) { // (1)
+			System.out.println(System.getProperty("user.dir") + File.separator + "ClipCon.exe");
+			runCommandAsAdmin("\"" + System.getProperty("user.dir") + File.separator + "ClipCon.exe" + "\""); // 이러면되나
+			System.exit(0);
+		}
+	}
+
+	/**
+	 * for test 17.09.13
+	 * 
+	 * @return Current Time YYYY-MM-DD HH:MM:SS
+	 */
 	public static String getTime() {
 		Date date = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd a hh:mm:ss");
 
 		return sdf.format(date).toString();
+	}
+
+	/** run a program at CMD */
+	public static void runCommandAsAdmin(String command) {
+		Elevator.executeAsAdmin("c:\\windows\\system32\\cmd.exe", "/C " + command);
 	}
 }
